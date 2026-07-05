6c4c134a5f10ae7193dcd13c318691e730a5a831
Strelchuk Serhiy <sstrelchuk382008@gmail.com>
2026-07-05 23:05:32 +0200

    feat(accounts): require first and last name on registration

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -7,3 +7,8 @@ class ClientRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["email", "first_name", "last_name", "phone"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["first_name"].required = True
+        self.fields["last_name"].required = True
