7697e9b955af5e43b18d0006be33a93b893128c7
Strelchuk Serhiy <sstrelchuk382008@gmail.com>
2026-07-10 20:46:12 +0200

    feat(booking): add booking creation view

--- backend/booking/urls.py
+++ backend/booking/urls.py
@@ -5,4 +5,5 @@ app_name = "booking"
 
 urlpatterns = [
     path("available-slots/", views.available_slots_view, name="available_slots"),
+    path("new/", views.booking_create_view, name="create")
 ]
--- backend/booking/views.py
+++ backend/booking/views.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.utils import timezone
 
 from accounts.models import Master
-from .services import get_available_slots
+
+from .forms import AppointmentForm
+from .services import get_available_slots, create_appointment
 
 
+@login_required
 def available_slots_view(request):
     master_id = request.GET.get("master_id")
     date_str = request.GET.get("date")
@@ -39,4 +44,25 @@ def available_slots_view(request):
                 for start, end in slots
             ]
         }
+    )
+
+
+@login_required
+def booking_create_view(request):
+    today = timezone.localdate()
+
+    if request.method == "POST":
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            create_appointment(
+                client=request.user,
+                master=form.cleaned_data["master"],
+                service_list=form.cleaned_data["services"],
+                start_at=form.cleaned_data["start_at"],
+            )
+            return redirect("home")
+        return render(request, "booking/create.html", {"form": form, "today": today})
+
+    return render(
+        request, "booking/create.html", {"form": AppointmentForm(), "today": today}
     )
